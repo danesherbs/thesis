a07593f7804f04d38c7d9b1ddd604a9d3d5d9018
danesherbs <danesherbs@gmail.com>
2017-02-23 17:45:50 +0000

    Finished 2D latent space CVAE

--- my_variational_autoencoder.py
+++ my_variational_autoencoder.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.layers import Input, Dense, Lambda, Flatten, Convolution2D, Deconvolution2D, Reshape
+from keras.layers import Input, Dense, Lambda, Convolution2D, Deconvolution2D
 from keras.models import Model
 from keras.datasets import mnist
 from keras import backend as K
 from keras import objectives
+from keras.callbacks import EarlyStopping
 import utils
 
 
@@ -27,7 +28,7 @@ def vae_loss(input_img, output_img):
 Define parameters
 '''
 batch_size = 32
-nb_epoch = 10
+nb_epoch = 1
 nb_filters = 8
 pool_size = (2, 2)
 kernal_size = (7, 7)
@@ -60,34 +61,34 @@ Define model
 '''
 input_img = Input(shape=input_shape)
 
-# 'kernal_size' convolution with 'nb_filters' output filters and stride 1x1 on a ?x? image
+# 'kernal_size' convolution with 'nb_filters' output filters and stride 1x1
 conv = Convolution2D(nb_filters, kernal_size[0], kernal_size[1], border_mode='valid', input_shape=input_shape)(input_img)
 
 # separate dense layers for mu and log(sigma), both of size latent_dim
-z_mean = Convolution2D(1, kernal_size[0], kernal_size[1], border_mode='valid')(conv)
-z_log_var = Convolution2D(1, kernal_size[0], kernal_size[1], border_mode='valid')(conv)
+z_mean = Convolution2D(nb_filters*2, kernal_size[0], kernal_size[1], border_mode='valid')(conv)
+z_log_var = Convolution2D(nb_filters*2, kernal_size[0], kernal_size[1], border_mode='valid')(conv)
 
 # sample from normal with z_mean and z_log_var
-z = Lambda(sampling, output_shape=(1, 30, 52))([z_mean, z_log_var])
+z_output_shape = output_shape=(nb_filters*2, 30, 52)
+z = Lambda(sampling, output_shape=z_output_shape)([z_mean, z_log_var])
 
-# 'kernal_size' transposed convolution with 32 output filters and stride 1x1 on a 8 16x16 images
-deconv = Deconvolution2D(nb_filters, kernal_size[0], kernal_size[1], output_shape=(None, 8, 36, 58), border_mode='valid', input_shape=(1, 30, 52))(z)
+# 'kernal_size' transposed convolution with 'nb_filters' output filters and stride 1x1
+deconv = Deconvolution2D(nb_filters, kernal_size[0], kernal_size[1], output_shape=(None, 8, 36, 58), border_mode='valid', input_shape=z_output_shape)(z)
 
 # 'kernal_size' transposed convolution with 1 output filter and stride 1x1 on a 32 22x22 images
 decoded_img = Deconvolution2D(1, kernal_size[0], kernal_size[1], output_shape=(None, 1, 42, 64), border_mode='valid', input_shape=(8, 36, 58))(deconv)
 
-# define model
+
+'''
+Train model
+'''
 encoder = Model(input_img, z)
 vae = Model(input_img, decoded_img)
-
 vae.summary()
 
-
-'''
-Compile and fit
-'''
-vae.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['binary_crossentropy'])
-vae.fit(X_train, X_train, validation_data=(X_test, X_test), batch_size=batch_size, nb_epoch=nb_epoch)
+vae.compile(loss=vae_loss, optimizer='adam')
+earlyStopping = EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
+vae.fit(X_train, X_train, validation_data=(X_test, X_test), batch_size=batch_size, nb_epoch=nb_epoch, shuffle=True, callbacks=[earlyStopping])
 
 
 '''
@@ -95,8 +96,12 @@ Compare reconstruction
 '''
 latent_imgs = encoder.predict(X_test)
 decoded_imgs = vae.predict(X_test)
+
 import matplotlib.pyplot as plt
 plt.matshow(X_test[0][0])
-plt.matshow(latent_imgs[0][0])
+plt.title('Original image')
+utils.show_subplot(latent_imgs[0])
+plt.title('Latent feature maps')
 plt.matshow(decoded_imgs[0][0])
+plt.title('Reconstructed image')
 plt.show()
