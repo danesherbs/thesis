984cbbf7ee86b61e90917fad04e4f126fa2bccbf
dgs13 <dgs13@ic.ac.uk>
2017-04-18 17:45:54 +0100

    Getting nan loss

--- cvae.py
+++ cvae.py
@@ -3,6 +3,8 @@ from keras.models import Model
 from keras import backend as K
 from keras import objectives
 from keras import losses
+from keras import optimizers
+from keras import initializers
 from keras.callbacks import EarlyStopping
 # import utils
 # import cPickle
@@ -35,10 +37,15 @@ def vae_loss(x, x_decoded_mean):
     kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
     return xent_loss + kl_loss
 
+def my_loss_function(y_true, y_pred):
+    return 1.0
+
+
 '''
 Define parameters
 '''
-batch_size = 64
+weight_seed = 123
+batch_size = 128
 epochs = 1
 filters = 8
 latent_filters = 4
@@ -74,53 +81,43 @@ print('X_test shape:', X_test.shape)
 '''
 Define model
 '''
+# weight initaliser
+kernel_initializer = initializers.TruncatedNormal(mean=0.0, stddev=0.5, seed=weight_seed)
+bias_initializer = initializers.TruncatedNormal(mean=1.0, stddev=0.5, seed=weight_seed)
+
 # define input with 'channels_first'
 input_img = Input(shape=input_shape)
 
 # 'kernal_size' convolution with 'filters' output filters, stride 1x1 and 'valid' border_mode
-conv2D_1 = Conv2D(filters, kernal_size, activation='relu', name='encoder_conv2D_1')(input_img)
+conv2D_1 = Conv2D(filters, kernal_size, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer, name='encoder_conv2D_1')(input_img)
 max_pooling_1 = MaxPooling2D(pool_size, name='encoder_max_pooling_1')(conv2D_1)
 
 # separate dense layers for mu and log(sigma), both of size latent_dim
-z_mean = Conv2D(latent_filters, kernal_size, name='encoder_z_mean')(max_pooling_1)
-z_log_sigma = Conv2D(latent_filters, kernal_size, name='encoder_z_log_sigma')(max_pooling_1)
+z_mean = Conv2D(latent_filters, kernal_size, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer, name='encoder_z_mean')(max_pooling_1)
+z_log_sigma = Conv2D(latent_filters, kernal_size, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer, name='encoder_z_log_sigma')(max_pooling_1)
 
 # sample from normal with z_mean and z_log_sigma
 # z_output_shape = output_shape=(latent_filters, 30, 52)
 z = Lambda(sampling, name='latent_space')([z_mean, z_log_sigma])
 
 # 'kernal_size' transposed convolution with 'filters' output filters and stride 1x1
-conv2DT_1 = Conv2DTranspose(filters, kernal_size, name='decoder_conv2DT_1')(z)
+conv2DT_1 = Conv2DTranspose(filters, kernal_size, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer, name='decoder_conv2DT_1')(z)
 up_sampling_1 = UpSampling2D(pool_size, name='decoder_up_sampling_1')(conv2DT_1)
-conv2DT_2 = Conv2DTranspose(1, kernal_size, name='decoder_conv2DT_2')(up_sampling_1)
+conv2DT_2 = Conv2DTranspose(1, kernal_size, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer, name='decoder_conv2DT_2')(up_sampling_1)
 
 # define decoded image to be image in last layer
 decoded_img = conv2DT_2
 
+
 '''
 Train model
 '''
-
 # define and save models
 vae = Model(input_img, decoded_img)
-encoder = Model(input=vae.input, output=vae.get_layer('latent_space').output)
-# saved_models = 'saved_models/'
-# description = '_epoch=' + str(epochs) + '_beta=' + str(int(beta)) + '_latent=' + str(latent_filters)
-# vae.save(saved_models + 'cvae' + description + '.h5')
-# encoder.save(saved_models + 'encoder' + description + '.h5')
 
 # print model summary
 vae.summary()
 
 # compile and train
-# vae.compile(loss=vae_loss, optimizer='adam', metrics=['binary_crossentropy'])
-vae.compile(loss=losses.mean_squared_error, optimizer='adam', metrics=['binary_crossentropy'])
-earlyStopping = EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
-history = vae.fit(X_train, X_train, validation_data=(X_test, X_test), batch_size=batch_size, epochs=epochs, callbacks=[earlyStopping])
-
-# # save learnt weights
-# vae.save_weights(saved_models + 'cvae' + description + '_weights' + '.h5')
-# encoder.save_weights(saved_models + 'encoder' + description + '_weights' + '.h5')
-#
-# # save history
-# cPickle.dump(saved_models + 'history' + description + '.pickle', 'w')
+vae.compile(loss=losses.mean_squared_error, optimizer='rmsprop')
+vae.fit(X_train, X_train, validation_data=(X_test, X_test), batch_size=batch_size, epochs=epochs)
