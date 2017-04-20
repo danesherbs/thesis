ea8036ecb32aef09c07d3ab3074518649853b451
danesherbs <danesherbs@gmail.com>
2017-04-20 17:04:51 +0100

    Added hyperparameter string builder

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ Utilities package for autoencoders
 '''
 
 from sklearn.model_selection import train_test_split
-from skimage.measure import block_reduce
+# from skimage.measure import block_reduce
 from matplotlib import pyplot as plt
 from keras import backend as K
 from keras import objectives
@@ -12,11 +12,12 @@ import numpy as np
 import os
 
 
-'''
-Contains screen-grabs from most recent run of ALE
-'''
-RECORD_PATH = './atari_agents/record/'
 
+'''
+Loading ALE data
+'''
+# Contains screen-grabs from most recent run of ALE
+RECORD_PATH = './atari_agents/record/'
 
 def image_to_array(image_path, rgb=False):
     '''
@@ -33,7 +34,7 @@ def __example_image_to_array():
     '''
     image_path = './atari_agents/record/020377.png'
     image_array = image_to_array(image_path, rgb=False)
-    print image_array
+    print(image_array)
     Image.fromarray(np.uint8(image_to_array(image_path))).show()
 
 def array_to_image(image_array):
@@ -56,7 +57,7 @@ def load_data(down_sample=False):
     Makes (X_train, X_test, y_train, y_test) from images in RECORD_PATH
     '''
     X = []
-    print 'Loading training data...',
+    print('Loading training data...'),
     for filename in os.listdir(RECORD_PATH):
     # for filename in os.listdir(RECORD_PATH)[::100]:
         if not filename.endswith('.png'):
@@ -67,11 +68,27 @@ def load_data(down_sample=False):
             X.append(down_sampled)
         else:
             X.append(image)
-    print 'done.'
+    print('done.')
     X = np.asarray(X, dtype='uint8')
     Y = np.asarray([-1]*len(X))  # psuedo labels
     return train_test_split(X, Y, test_size=0.10)
 
+
+'''
+Hyperparameter searching
+'''
+def build_hyperparameter_string(name, hp_dictionary):
+    hyperparameters = sorted(hp_dictionary.keys())
+    out_string = name
+    for hp in hyperparameters:
+        hp_value = hp_dictionary[hp]
+        out_string += "_" + str(hp) + "_" + str(hp_value)
+    return out_string
+
+
+'''
+Graphing utilities
+'''
 def show_subplot(images):
     '''
     Plots monochrome images in a subplot figure
@@ -91,22 +108,6 @@ def __example_show_subplot():
     imgs = pickle.load(open('encoded_imgs.pickle', 'rb'))
     show_subplot(imgs)
 
-def vae_loss(input_img, output_img, mean, log_var, beta=1.0):
-        # compute the reconstruction loss (binary cross entropy)
-        reconstruction_loss = objectives.binary_crossentropy(input_img.flatten(), output_img.flatten())
-        # compute the KL divergence between approximate and latent posteriors
-        kl_loss = - 0.5 * K.sum(1 + log_var - K.square(mean) - K.exp(log_var), axis=-1)
-        return reconstruction_loss + beta * kl_loss
-
-def sampling(args, latent_dim):
-        '''
-        Inputs: mean and std vectors
-        Output: sampled vector
-        '''
-        z_mean, z_log_var = args  # unpack args
-        epsilon = K.random_normal(shape=(latent_dim,), mean=0.0, std=1.0)  # sample from standard normal
-        return z_mean + K.exp(z_log_var / 2) * epsilon
-
 '''
 MAIN
 '''
@@ -114,4 +115,5 @@ if __name__ == '__main__':
     # input_img = image_to_array('./atari_agents/record/020377.png', rgb=False)
     # output_img = image_to_array('./atari_agents/record/000170.png', rgb=False)
     # print vae_loss(input_img, output_img)
-    load_data()
+    # load_data()
+    pass
