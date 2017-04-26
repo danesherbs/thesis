25d7d84b8459f35238a19754e11dcac82cd7738a
dgs13 <dgs13@ic.ac.uk>
2017-04-26 21:00:50 +0100

    Saving current work on CVAE

--- CVAE.py
+++ CVAE.py
@@ -1,10 +1,11 @@
 from keras import initializers
+from keras.datasets import mnist
 import utils
 
 
 class CVAE(object):
     '''
-    Convolutional Variational Autoencoder
+    Convolutional Variational Autoencoder (CVAE)
     '''
 
     def __init__(self,
@@ -37,12 +38,18 @@ class CVAE(object):
         self.kernel_initializer = initializers.TruncatedNormal(mean=init_kernel_mean, stddev=init_kernal_stddev, seed=weight_seed)
         self.bias_initializer = initializers.TruncatedNormal(mean=init_bias_mean, stddev=init_bias_stddev, seed=weight_seed)
 
+        # load MNIST
+        self.mnist = self.__load_mnist_images(down_sample = True)
+
         filters = 8
         latent_filters = 4
         kernal_size = (3, 3)
         pool_size = (2, 2)
 
     def __build_log_dir(self, filename):
+        '''
+        Builds log directory (where summaries are stored)
+        '''
         # builder hyperparameter dictionary
         hp_dictionary = {
         	'batch_size': self.batch_size,
@@ -54,6 +61,34 @@ class CVAE(object):
         # define log directory
         return './summaries/' + utils.build_hyperparameter_string(filename, hp_dictionary) + '/'
 
+    def __load_mnist_images(self, down_sample = False):
+        '''
+        Loads MNIST images (X_train, X_test) normalised between [0, 1]
+        '''
+        # load MNIST images only
+        (X_train, _), (X_test, _) = mnist.load_data()
+
+        if down_sample:
+            X_train = X_train[::20]
+            X_test = X_test[::20]
+
+        # reshape into (num_samples, num_channels, width, height)
+        X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1], X_train.shape[2])
+        X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1], X_test.shape[2])
+
+        # record input shape
+        input_shape = X_train.shape[1:]
+
+        # cast pixel values to floats
+        X_train = X_train.astype('float32')
+        X_test = X_test.astype('float32')
+
+        # normalise pixel values
+        X_train /= 255.0
+        X_test /= 255.0
+
+        # return normalise MNIST images
+        return X_train, X_test
 
     def encoder(self):
         pass
