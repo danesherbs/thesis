67bb0d87c25246bbcfff02d51c681398aef40191
danesherbs <danesherbs@gmail.com>
2017-05-02 17:46:12 +0100

    Changed intermediate acivation functions

--- utils.py
+++ utils.py
@@ -17,14 +17,14 @@ def image_to_array(image_path, rgb=False):
     '''
     image = Image.open(image_path)
     if not rgb:
-        image = image.convert('L')  # convert to black and white
-    return np.asarray(image, dtype=np.uint8)  # uint8 \in [0,255]
+        image = image.convert('L')  # extract luminance
+    return np.asarray(image)
 
 def __example_image_to_array():
     '''
     Takes random screen-grab in RECORD_PATH and shows array and image
     '''
-    image_path = './atari_agents/record/020377.png'
+    image_path = './atari_agents/record/0.png'
     image_array = image_to_array(image_path, rgb=False)
     print(image_array)
     Image.fromarray(np.uint8(image_to_array(image_path))).show()
@@ -44,7 +44,7 @@ def __example_array_to_image():
     image = array_to_image(image_array)
     image.show()
 
-def load_data(down_scale_factor=0.5):
+def load_data():
     '''
     Makes (X_train, X_test, y_train, y_test) from images in RECORD_PATH
     '''
@@ -56,22 +56,23 @@ def load_data(down_scale_factor=0.5):
     for filename in os.listdir(RECORD_PATH):
         if not filename.endswith('.png'):
             continue  # skip non-png files
-        image_array = image_to_array(os.path.join(RECORD_PATH, filename))
-        down_sampled = imresize(image_array, down_scale_factor)
-        X.append(down_sampled)
-    X = np.asarray(X, dtype='uint8')
+        image = Image.open(os.path.join(RECORD_PATH, filename))
+        image = image.convert('L')
+        image = image.resize((84, 84), Image.ANTIALIAS)
+        X.append(np.asarray(image))
+    X = np.asarray(X)
     y = np.asarray([-1]*len(X))  # psuedo labels
     X_train, X_test, _, _ = train_test_split(X, y, test_size=0.10, random_state=42)
     return (X_train, None), (X_test, None)
 
 def __demo_plot_data():
     # load data and print shape
-    (X_train, _), (X_test, _) = load_data(down_scale_factor=0.5)
+    (X_train, _), (X_test, _) = load_data()
     print(X_train.shape)
     # plot sample image
     import matplotlib.pyplot as plt
     plt.figure()
-    plt.imshow(X_train[1], cmap='inferno')
+    plt.imshow(X_train[30], cmap='inferno')
     plt.show()
 
 
@@ -92,32 +93,13 @@ def __date_and_time_string():
 
 
 '''
-Graphing utilities
-'''
-def show_subplot(images):
-    '''
-    Plots monochrome images in a subplot figure
-    '''
-    from matplotlib import pyplot as plt
-    nrows = 4
-    ncols = len(images) / nrows
-    fig, axs = plt.subplots(nrows=nrows, ncols=ncols)
-    for i in xrange(nrows):
-        for j in xrange(ncols):
-            if ncols * i + j < len(images):
-                axs[i][j].matshow(images[nrows+ncols])
-    plt.tight_layout()
-    plt.show()
-
-def __example_show_subplot():
-    import cPickle as pickle
-    imgs = pickle.load(open('encoded_imgs.pickle', 'rb'))
-    show_subplot(imgs)
-
-'''
 MAIN
 '''
 if __name__ == '__main__':
-    # input_img = image_to_array('./atari_agents/record/020377.png', rgb=False)
-    # output_img = image_to_array('./atari_agents/record/000170.png', rgb=False)
+
     __demo_plot_data()
+
+    # image_path = './atari_agents/record/100.png'
+    # image_array = image_to_array(image_path, rgb=False)
+    # print(np.min(image_array))
+    # print(np.max(image_array))
