6fb38c9ac21c02ff3d4bea37bf4cb3c9594e2b38
danesherbs <danesherbs@gmail.com>
2017-05-11 19:38:16 +0100

    Moved tutorials to misc

---
+++ miscellaneous/tutorials/subplot_example.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy.random as rnd
+
+
+fig = plt.figure()
+
+plt.subplot(221)
+plt.imshow(rnd.random((100, 100)))
+plt.subplot(222)
+plt.imshow(rnd.random((100, 100)))
+plt.subplot(223)
+plt.imshow(rnd.random((100, 100)))
+plt.subplot(224)
+plt.imshow(rnd.random((100, 100)))
+
+plt.show()
