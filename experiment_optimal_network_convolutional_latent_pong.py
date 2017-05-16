0adec227e43d8682205e64b4293e2f6cbae4dabf
danesherbs <danesherbs@gmail.com>
2017-05-16 13:51:46 +0100

    Trying conv ltent witohtou batchnorm

--- experiment_optimal_network_convolutional_latent_pong.py
+++ experiment_optimal_network_convolutional_latent_pong.py
@@ -6,11 +6,14 @@ This is done so that we may see if objects may be recognised by the existing met
 Dataset: 300,000 screenshots of Pong, 10% used for validation.
 '''
 
-from cvae_atari import PongEntangledConvolutionalLatentVAE
+from cvae_atari import PongEntangledConvolutionalLatentVAE, \
+					   PongEntangledConvolutionalLatentNoBatchNormVAE
 import sampling
 import utils
 import numpy as np
 
+
+experiment = 'experiment_optimal_network_convolutional_latent_pong'
 
 
 def train_only_reconstruction_loss_pong_network():
@@ -37,7 +40,7 @@ def train_only_reconstruction_loss_pong_network():
 	}
 
 	# define log directory
-	log_dir = './summaries/experiment_optimal_network_convolutional_latent_pong/' + utils.build_hyperparameter_string(name, hp_dictionary) + '/'
+	log_dir = './summaries/' + experiment + '/' + utils.build_hyperparameter_string(name, hp_dictionary) + '/'
 
 	# make VAE
 	vae = PongEntangledConvolutionalLatentVAE(input_shape, 
@@ -96,7 +99,7 @@ def train_entangled_pong_network():
 	}
 
 	# define log directory
-	log_dir = './summaries/experiment_optimal_network_convolutional_latent_pong/' + utils.build_hyperparameter_string(name, hp_dictionary) + '/'
+	log_dir = './summaries/' + experiment + '/' + utils.build_hyperparameter_string(name, hp_dictionary) + '/'
 
 	# make VAE
 	vae = PongEntangledConvolutionalLatentVAE(input_shape, 
@@ -157,13 +160,73 @@ def train_entangled_pong_network_with_image_latent_space():
 	}
 
 	# define log directory
-	log_dir = './summaries/experiment_optimal_network_convolutional_latent_pong/' + utils.build_hyperparameter_string(name, hp_dictionary) + '/'
+	log_dir = './summaries/' + experiment + '/' + utils.build_hyperparameter_string(name, hp_dictionary) + '/'
 
 	# make VAE
 	vae = PongEntangledConvolutionalLatentVAE(input_shape, 
 								            log_dir,
 								            filters=filters,
 								            latent_filters=latent_filters,
+								            kernel_size=kernel_size,
+								            beta=beta)
+
+	# compile VAE
+	from keras import optimizers
+	optimizer = optimizers.Adam(lr=1e-1)
+	vae.compile(optimizer=optimizer)
+
+	# get dataset
+	train_directory = './atari_agents/record/train/'
+	test_directory = './atari_agents/record/test/'
+	train_generator = utils.atari_generator(train_directory, batch_size=batch_size)
+	test_generator = utils.atari_generator(test_directory, batch_size=batch_size)
+	train_size = utils.count_images(train_directory)
+	test_size = utils.count_images(test_directory)
+
+	# print summaries
+	vae.print_model_summaries()
+
+	# fit VAE
+	steps_per_epoch = int(train_size / batch_size)
+	validation_steps = int(test_size / batch_size)
+	vae.fit_generator(train_generator,
+	               epochs=epochs,
+	               steps_per_epoch=steps_per_epoch,
+	               validation_data=test_generator,
+	               validation_steps=validation_steps)
+
+
+
+def train_only_reconstruction_loss_pong_network_no_batch_norm():
+	# inputs
+	input_shape = (1, 84, 84)
+	filters = 32
+	kernel_size = 6
+	beta = 0.0  # only reconstruction loss
+	epochs = 10
+	batch_size = 1
+
+	# define filename
+	name = 'cvae_atari_only_reconstruction_loss_pong_no_batch_norm'
+
+	# builder hyperparameter dictionary
+	hp_dictionary = {
+	    'epochs': epochs,
+	    'batch_size': batch_size,
+	    'beta': beta,
+	    'filters': filters,
+	    'kernel_size': kernel_size,
+	    'loss': 'vae_loss',
+	    'optimizer': 'adam'
+	}
+
+	# define log directory
+	log_dir = './summaries/' + experiment + '/' + utils.build_hyperparameter_string(name, hp_dictionary) + '/'
+
+	# make VAE
+	vae = PongEntangledConvolutionalLatentNoBatchNormVAE(input_shape, 
+								            log_dir,
+								            filters=filters,
 								            kernel_size=kernel_size,
 								            beta=beta)
 
@@ -204,7 +267,6 @@ def main():
 	batch_size = 1
 
 	# log directory
-	experiment = 'experiment_optimal_network_convolutional_latent_pong'
 	run = 'cvae_atari_entangled_pong_with_latent_image_15_May_01_05_19_batch_size_1_beta_1.0_epochs_100_filters_32_kernel_size_6_latent_filters_1_loss_vae_loss_optimizer_adam'
 	log_dir = './summaries/' + experiment + '/' + run + '/'
 
@@ -242,4 +304,5 @@ if __name__ == '__main__':
 	# train_only_reconstruction_loss_pong_network()
 	# train_entangled_pong_network()
 	# train_entangled_pong_network_with_image_latent_space()
-	main()
+	train_only_reconstruction_loss_pong_network_no_batch_norm()
+	# main()
