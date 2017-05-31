4ebdeaad87a2417a9f35a08fd609f8d032acf8aa
danesherbs <danesherbs@gmail.com>
2017-05-31 22:43:49 +0100

    Adding experiment

--- experiment_dense_autoencoder.py
+++ experiment_dense_autoencoder.py
@@ -1,6 +1,8 @@
 from cvae_mnist import DenseAutoencoder
 from keras import optimizers
 import utils
+import sampling
+import matplotlib.pyplot as plt
 
 
 experiment = 'experiment_dense_autoencoder'
@@ -54,5 +56,51 @@ def train_dense_autoencoder():
                    validation_steps=validation_steps)
 
 
+def main():
+    # inputs
+    img_channels = 1
+    input_shape = (img_channels, 28, 28)
+    epochs = 50
+    batch_size = 1
+    lr = 1e-4
+
+    # define filename
+    name = 'autoencoder_mnist'
+
+    # builder hyperparameter dictionary
+    hp_dictionary = {
+        'epochs': epochs,
+        'batch_size': batch_size,
+        'lr': lr,
+        'loss': 'vae_loss',
+        'optimizer': 'adam'
+    }
+
+    # define log directory
+    run = 'autoencoder_mnist_31_May_19_54_30_batch_size_1_epochs_60_loss_vae_loss_lr_0.0001_optimizer_adam'
+    log_dir = './summaries/' + experiment + '/' + run + '/'
+
+    # make VAE
+    autoencoder = DenseAutoencoder(input_shape, log_dir)
+    
+    # load weights
+    autoencoder.load_model()
+
+    # extract models
+    model = autoencoder.get_model()
+    decoder = autoencoder.get_decoder()
+    encoder = autoencoder.get_encoder()
+    
+    # get dataset
+    (X_train, _), (X_test, _) = utils.load_mnist()
+    train_generator = utils.make_generator(X_train, batch_size=batch_size)
+    test_generator = utils.make_generator(X_test, batch_size=batch_size)
+    
+    # show original and reconstruction
+    sampling.encode_decode_sample(X_test, model, input_shape=(1, 28, 28))
+    plt.show()
+
+
 if __name__ == '__main__':
-    train_dense_autoencoder()
+    # train_dense_autoencoder()
+    main()
